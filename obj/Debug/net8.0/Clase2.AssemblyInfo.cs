65d500c589e1a4a7e6c2cdfeb5018fa284fdaded
Juan Pablo Bedoya <juan.pablo.bedoya02@gmail.com>
2024-04-25 19:31:50 -0500

    figonachi

--- obj/Debug/net8.0/Clase2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Clase2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clase2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab6c774d72bd8b218511cbb973fb80dfdb6b63d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece87e4491643416c24e60c8c2863e303450666a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clase2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clase2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
